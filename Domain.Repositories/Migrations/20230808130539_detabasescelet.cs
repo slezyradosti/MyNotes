6f83cbc5e74512713659d255fbd3925d5c40d249
Life is squid <rredzer000@gmail.com>
2023-08-08 15:37:11 +0100

    created basic entities
    update entity: Notebook
    created entities: unit, page, note
    created migration

---
+++ Domain.Repositories/Migrations/20230808130539_detabasescelet.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class detabasescelet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_notebooks",
+                table: "notebooks");
+
+            migrationBuilder.RenameTable(
+                name: "notebooks",
+                newName: "Notebooks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notebooks",
+                table: "Notebooks",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Units",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Timestamp = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Units", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Units_Notebooks_Id",
+                        column: x => x.Id,
+                        principalTable: "Notebooks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Timestamp = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pages_Units_Id",
+                        column: x => x.Id,
+                        principalTable: "Units",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Record = table.Column<string>(type: "text", nullable: false),
+                    Timestamp = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Notes_Pages_Id",
+                        column: x => x.Id,
+                        principalTable: "Pages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+
+            migrationBuilder.DropTable(
+                name: "Pages");
+
+            migrationBuilder.DropTable(
+                name: "Units");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Notebooks",
+                table: "Notebooks");
+
+            migrationBuilder.RenameTable(
+                name: "Notebooks",
+                newName: "notebooks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_notebooks",
+                table: "notebooks",
+                column: "Id");
+        }
+    }
+}
