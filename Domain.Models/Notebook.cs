ba5f8fa41cb94efa5537bd87556f384a789be5d4
Life is squid <rredzer000@gmail.com>
2023-08-08 10:27:41 +0100

    Big configuration update
    Update class Program
    Created class Startup
    
    Established db connection
    Created entity notebook as well as first migration

---
+++ Domain.Models/Notebook.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models
+{
+    public class Notebook : BaseEntity
+    {
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
