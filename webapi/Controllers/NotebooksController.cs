35812995789d0a5ea7c2e032f152a0374554a96a
Life is squid <rredzer000@gmail.com>
2023-08-10 17:09:55 +0100

    created Base controller
    the controller contains mediator as a protected field. So no needs to inject mediator to every controller

--- webapi/Controllers/NotebooksController.cs
+++ webapi/Controllers/NotebooksController.cs
@@ -5,21 +5,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace webapi.Controllers
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class NotebooksController : ControllerBase
+    public class NotebooksController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public NotebooksController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet]
         public async Task<List<Notebook>> List()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
     }
 }
