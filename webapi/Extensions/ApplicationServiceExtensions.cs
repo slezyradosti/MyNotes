d7476532099baa209b74f1d585ca1af3961a0acb
Life is squid <rredzer000@gmail.com>
2023-08-10 13:30:05 +0100

    adding CORS
    added default and 'any' policies

--- webapi/Extensions/ApplicationServiceExtensions.cs
+++ webapi/Extensions/ApplicationServiceExtensions.cs
@@ -9,6 +9,22 @@ namespace webapi.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services,
             IConfiguration configuration)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(cfg =>
+                {
+                    cfg.WithOrigins(configuration["AllowedOrigins"]);
+                    cfg.AllowAnyHeader();
+                    cfg.AllowAnyMethod();
+                });
+                options.AddPolicy(name: "AnyOrigin", cfg =>
+                {
+                    cfg.AllowAnyOrigin();
+                    cfg.AllowAnyHeader();
+                    cfg.AllowAnyMethod();
+                });
+            });
+
             services.AddScoped<INotebookRepository, NotebookRepository>();
 
             return services;
