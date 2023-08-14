3c0987d3f00a6acbe965b5e3a2615d12b71bfa39
Life is squid <rredzer000@gmail.com>
2023-08-14 18:22:30 +0100

    Unit controller small corrections
    Foreing Key 'notebookId' now must be in Body request

--- webapi/Controllers/UnitsController.cs
+++ webapi/Controllers/UnitsController.cs
@@ -1,6 +1,5 @@
 ﻿using Application.DTOs;
 using Application.Units;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 
 namespace webapi.Controllers
@@ -13,28 +12,27 @@ namespace webapi.Controllers
             return HandleResult(await Mediator.Send(new List.Query { NotebookId = nbId }));
         }
 
-        [HttpGet("{id}&nbId")]
+        [HttpGet("{id}")]
         public async Task<IActionResult> GetUnit(Guid id)
         {
             return HandleResult(await Mediator.Send(new Details.Query { UnitId = id }));
         }
 
-        [HttpPost("nbId")]
-        public async Task<IActionResult> CreateUnit(Guid nbId, UnitDto unit)
+        [HttpPost]
+        public async Task<IActionResult> CreateUnit(UnitDto unit)
         {
-            unit.NotebookId = nbId;
+            //unit.NotebookId = nbId;
             return HandleResult(await Mediator.Send(new Create.Command { Unit = unit }));
         }
 
-        [HttpPut("{id}&nbId")]
-        public async Task<IActionResult> EditUnit(Guid id, Guid nbId, UnitDto unit)
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditUnit(Guid id, UnitDto unit)
         {
             unit.Id = id;
-            unit.NotebookId = nbId;
             return HandleResult(await Mediator.Send(new Edit.Command { Unit = unit }));
         }
 
-        [HttpDelete("{id}&nbId")]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUnit(Guid id)
         {
             return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
