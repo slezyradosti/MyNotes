1fc53bf789b58f1194428f46bd2c68ca232cc3df
Life is squid <rredzer000@gmail.com>
2023-10-27 17:48:35 +0100

    made most of the routes 'protected'
    now to visit particular routes user has to be authenticated, otherwise he/she will se login page

--- reactapp/src/app/router/Routes.tsx
+++ reactapp/src/app/router/Routes.tsx
@@ -5,32 +5,35 @@ import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
 import Dashboard from "../../features/dashboard/Dashboard";
 import ProfilePage from "../../features/profiles/ProfilePage";
+import RequireAuth from "./RequereAuth";
 
 export const routes: RouteObject[] = [
     {
         path: '/',
         element: <App />,
         children: [
+            {
+                element: <RequireAuth />,
+                children: [
+
+                    { path: 'notebooks', element: <Dashboard /> },
+                    { path: 'notebooks/:id', element: <Dashboard /> },
+                    { path: 'units', element: <Dashboard /> },
+                    { path: 'units/:id', element: <Dashboard /> },
+                    { path: 'pages', element: <Dashboard /> },
+                    { path: 'pages/:id', element: <Dashboard /> },
+                    { path: 'notes', element: <Dashboard /> },
+                    { path: 'notes/:id', element: <Dashboard /> },
+                    { path: 'profiles/:id', element: <ProfilePage /> }
+                ],
+            },
             { path: '', element: <HomePage /> },
             { path: 'login', element: <HomePage /> },
-            { path: 'notebooks', element: <Dashboard /> },
-            { path: 'notebooks/:id', element: <Dashboard /> },
-            { path: 'units', element: <Dashboard /> },
-            { path: 'units/:id', element: <Dashboard /> },
-            { path: 'pages', element: <Dashboard /> },
-            { path: 'pages/:id', element: <Dashboard /> },
-            { path: 'notes', element: <Dashboard /> },
-            { path: 'notes/:id', element: <Dashboard /> },
-            { path: 'profiles/:id', element: <ProfilePage /> },
             { path: 'not-found', element: <NotFound /> },
             { path: 'server-error', element: <ServerError /> },
             { path: '*', element: <Navigate replace to='/not-found' /> },
         ]
     },
-    // {
-    //     path: '/login',
-    //     element: <LoginForm />,
-    // }
 ]
 
 export const router = createBrowserRouter(routes)
