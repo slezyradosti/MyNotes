76d95118414424055eed9d129c6540818e741bcc
Life is squid <rredzer000@gmail.com>
2023-11-03 23:33:30 +0000

    xUnit testing first step

---
+++ TestingLogic/BaseApiControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Application.Core;
+using Microsoft.AspNetCore.Mvc;
+using webapi.Controllers;
+
+namespace TestingLogic
+{
+    public class BaseApiControllerTest : BaseApiController
+    {
+        [Fact]
+        public void NullResultTest()
+        {
+            var expected = NotFound().ToString();
+            var actual = HandleResult(Result<object>.Success(null)).ToString();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
